f5e8ece05c4e5b898231bda6f9f8ebd86616d88c
Mario Vercellotti <mario.vercellotti@wunderkraut.com>
2019-03-01 18:11:55 +0200

    Change the name and descriptions

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Umami gatsby demo`,
+    description: `This is a test demo implementation of gatsby + drupal`,
+    author: `@wunder`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
