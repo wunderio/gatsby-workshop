a336053537de50674d060cff651c3dc41b3b26ce
Mario Vercellotti <mario.vercellotti@wunderkraut.com>
2019-03-01 14:52:14 +0200

    Add and configure the drupal plugin to connect to the drupal backend

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,17 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-drupal`,
+      options: {
+        baseUrl: `http://master.gatsby-workshop-backend.silta.wdr.io`,
+        apiBase: `jsonapi`, // optional, defaults to `jsonapi`
+        basicAuth: {
+          username: "silta",
+          password: "demo",
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
